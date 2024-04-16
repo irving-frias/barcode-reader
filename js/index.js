74fe21194274b7cb679bce62a57bef23ee1c81cb
irving-frias <irvingfrias41@gmail.com>
2024-04-16 14:38:46 -0700

    Added

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
 function onScanSuccess(decodedText, decodedResult) {
   // Handle on success condition with the decoded text or result.
   document.querySelector('#result').innerText = decodedText;
+  speak(decodedText);
 }
+
+function speak(text) {
+  // Create a SpeechSynthesisUtterance
+  const utterance = new SpeechSynthesisUtterance(text);
+
+  // Select a voice
+  const voices = speechSynthesis.getVoices();
+  utterance.voice = voices[0]; // Choose a specific voice
+
+  // Speak the text
+  speechSynthesis.speak(utterance);
+}
+
 
 var html5QrcodeScanner = new Html5QrcodeScanner(
 "reader", { fps: 10, qrbox: 250 });
