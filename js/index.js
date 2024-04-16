673933193e6f2e55d291c71bdb22f01fbd4fd952
irving-frias <irvingfrias41@gmail.com>
2024-04-16 14:25:29 -0700

    Added more

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-  
-});
+function onScanSuccess(decodedText, decodedResult) {
+  // Handle on success condition with the decoded text or result.
+  document.querySelector('#result').innerText = decodedText;
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner(
+"reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess);
